9f6b4778671e583a877fe8fe711451448dcfaa5b
narkopolo <openrandonaut@riseup.net>
2022-08-03 00:19:53 +0200

    Bump version

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
 __author__ = 'openranonaut'
-__version__ = "0.1.0"
+__version__ = "0.1.1"
--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ elif argv[-1] == 'clean':
 
 setup(
     name="pyrandonaut",
-    version="0.1.0",
+    version="0.1.1",
     description="Open-source quantum random coordinate generation for randonauts.",
     long_description_content_type='text/markdown',
     long_description=long_description,
